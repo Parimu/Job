17480bfd3ae538aa0fe465d59756f3108c2ce911
Parimu <91951812+Parimu@users.noreply.github.com>
2022-11-17 22:13:01 +0000

    Update App.java

--- src/main/java/cucumber_Randox/Randox/App.java
+++ src/main/java/cucumber_Randox/Randox/App.java
@@ -14,17 +14,6 @@ public class App
         System.out.println("Welcome to Selenium2");
         System.out.println("updated on 17/11/2022");
          System.out.println("updated on 17/11/2022");
-         //updated on 17/11/2022 @18.12pm
-
-        ////updated on 17/11/2022 @18.14pm
-         ////updated on 17/11/2022 @18.14pm
-         
-     ////updated on 17/11/2022 @18.21pm
-         
-         // Switched to develop branch on 17/11/2022 @19.07pm
-         //updated changes in develop branch on 17/11/2022 @ 19.29pm
-         
-
-        //again pulling from develop branch @19.31pm
+        
     }
 }
